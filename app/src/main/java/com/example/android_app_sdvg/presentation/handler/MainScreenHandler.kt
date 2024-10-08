46799d6a50a6e470db7a939238693507c71b5689
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-10-08 16:55:35 +0500

    Create screens

---
+++ app/src/main/java/com/example/android_app_sdvg/presentation/handler/MainScreenHandler.kt
@@ -0,0 +1,12 @@
+package com.example.android_app_sdvg.presentation.handler
+
+interface MainScreenHandler {
+    fun onToPersonalAccount()
+}
+
+class MainScreenHandlerImpl : MainScreenHandler{
+    override fun onToPersonalAccount() {
+        TODO("Not yet implemented")
+    }
+
+}
