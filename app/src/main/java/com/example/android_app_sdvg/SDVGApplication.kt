785b653c0172b775c18bba5590a13d87fbc743af
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-10-10 15:58:38 +0500

    Edit app

--- app/src/main/java/com/example/android_app_sdvg/SDVGApplication.kt
+++ app/src/main/java/com/example/android_app_sdvg/SDVGApplication.kt
@@ -5,8 +5,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.android_app_sdvg.presentation.handler.MainScreenHandlerImpl
-import com.example.android_app_sdvg.presentation.screen.MainScreen
+import com.example.android_app_sdvg.presentation.navigation.graph.BottomNavigationBarGraph
+import com.example.android_app_sdvg.presentation.screen.BottomBarScreen
+import com.example.android_app_sdvg.presentation.screen.CalendarScreen
+import com.example.android_app_sdvg.presentation.screen.ClickerScreen
 import com.example.android_app_sdvg.presentation.screen.Screen
 import dagger.hilt.android.HiltAndroidApp
 
@@ -20,15 +22,6 @@ class SDVGApplication : Application() {
     @Composable
     fun SDVGNavHost() {
         val navController = rememberNavController()
-        NavHost(
-            navController = navController,
-            startDestination = Screen.Main.route
-        ) {
-            composable(route = Screen.Main.route) {
-                MainScreen(
-                    MainScreenHandlerImpl()
-                )
-            }
-        }
+        BottomBarScreen(navController = navController)
     }
 }
