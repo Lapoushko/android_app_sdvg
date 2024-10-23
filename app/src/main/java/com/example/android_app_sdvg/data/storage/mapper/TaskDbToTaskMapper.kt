384f5e73b52710c5e05b714c02bdcc46b54c02a8
Lapoushko <vadim.dovgiy@mail.ru>
2024-10-24 00:40:17 +0500

    new mappers

--- app/src/main/java/com/example/android_app_sdvg/data/storage/mapper/TaskDbToTaskMapper.kt
+++ app/src/main/java/com/example/android_app_sdvg/data/storage/mapper/TaskDbToTaskMapper.kt
@@ -4,27 +4,38 @@ import com.example.android_app_sdvg.data.storage.entity.TaskDb
 import com.example.android_app_sdvg.domain.entity.category.Category
 import com.example.android_app_sdvg.domain.entity.prioriry.Priority
 import com.example.android_app_sdvg.domain.entity.task.Task
-import com.example.android_app_sdvg.presentation.model.task.TaskItem
+import javax.inject.Inject
 
 /**
  * @author Lapoushko
+ *
+ * Маппер для перевода таски из бд в обычную таску из domain
  */
 interface TaskDbToTaskMapper {
-    suspend fun invoke(task: TaskDb): Task
+    /**
+     * Изменить тип данных
+     * @param tasks задачи
+     * @return Список задач domain
+     */
+    suspend fun invoke(tasks: List<TaskDb>): List<Task>
 }
 
-class TaskDbToTaskMapperImpl(): TaskDbToTaskMapper{
-    override suspend fun invoke(task: TaskDb): Task {
-        return Task(
-            name = task.name ?: "Нет названия",
-            description = task.description ?: "Нет описания",
-            timer = task.timer ?: 0L,
-            dateStart = task.dateStart ?: 0L,
-            capacity = task.capacity ?: 0L,
-            periodicity = task.periodicity ?: 0,
-            priority = task.priority ?: Priority.HIGH,
-            category = task.category ?: Category.STANDART
-        )
+/**
+ * Реализация одноименного интерфейса
+ */
+class TaskDbToTaskMapperImpl @Inject constructor() : TaskDbToTaskMapper {
+    override suspend fun invoke(tasks: List<TaskDb>): List<Task> {
+        return tasks.map { task ->
+            Task(
+                name = task.name ?: "Нет названия",
+                description = task.description ?: "Нет описания",
+                timer = task.timer ?: 0L,
+                dateStart = task.dateStart ?: 0L,
+                capacity = task.capacity ?: 0L,
+                periodicity = task.periodicity ?: 0,
+                priority = task.priority ?: Priority.HIGH,
+                category = task.category ?: Category.STANDART
+            )
+        }
     }
-
 }
