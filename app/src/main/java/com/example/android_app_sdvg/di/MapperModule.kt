6763fa0137995290f4ec8e97302212855b5b9b1f
Lapoushko <vadim.dovgiy@mail.ru>
2024-11-29 15:23:22 +0500

    new modules

--- app/src/main/java/com/example/android_app_sdvg/di/MapperModule.kt
+++ app/src/main/java/com/example/android_app_sdvg/di/MapperModule.kt
@@ -1,13 +1,17 @@
 package com.example.android_app_sdvg.di
 
-import com.example.android_app_sdvg.data.storage.mapper.TaskDbToTaskMapper
-import com.example.android_app_sdvg.data.storage.mapper.TaskDbToTaskMapperImpl
-import com.example.android_app_sdvg.data.storage.mapper.TaskToTaskDbMapper
-import com.example.android_app_sdvg.data.storage.mapper.TaskToTaskDbMapperImpl
-import com.example.android_app_sdvg.presentation.mapper.TaskToUiMapper
-import com.example.android_app_sdvg.presentation.mapper.TaskToUiMapperImpl
-import com.example.android_app_sdvg.presentation.mapper.TaskUiToTaskMapper
-import com.example.android_app_sdvg.presentation.mapper.TaskUiToTaskMapperImpl
+import com.example.android_app_sdvg.data.storage.mapper.profile.ProfileMapperDB
+import com.example.android_app_sdvg.data.storage.mapper.profile.ProfileMapperDBImpl
+import com.example.android_app_sdvg.data.storage.mapper.task.TaskDbToTaskMapper
+import com.example.android_app_sdvg.data.storage.mapper.task.TaskDbToTaskMapperImpl
+import com.example.android_app_sdvg.data.storage.mapper.task.TaskToTaskDbMapper
+import com.example.android_app_sdvg.data.storage.mapper.task.TaskToTaskDbMapperImpl
+import com.example.android_app_sdvg.presentation.mapper.profile.ProfileMapperUI
+import com.example.android_app_sdvg.presentation.mapper.profile.ProfileMapperUIImpl
+import com.example.android_app_sdvg.presentation.mapper.task.TaskDomainToUiMapper
+import com.example.android_app_sdvg.presentation.mapper.task.TaskDomainToUiMapperImpl
+import com.example.android_app_sdvg.presentation.mapper.task.TaskUiToTaskDomainMapper
+import com.example.android_app_sdvg.presentation.mapper.task.TaskUiToTaskDomainMapperImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -23,25 +27,37 @@ import javax.inject.Singleton
 object MapperModule {
     @Singleton
     @Provides
-    fun provideTaskDbToTaskMapper(): TaskDbToTaskMapper{
+    fun provideTaskDbToTaskMapper(): TaskDbToTaskMapper {
         return TaskDbToTaskMapperImpl()
     }
 
     @Singleton
     @Provides
-    fun provideTaskToUiMapper(): TaskToUiMapper{
-        return TaskToUiMapperImpl()
+    fun provideTaskToUiMapper(): TaskDomainToUiMapper {
+        return TaskDomainToUiMapperImpl()
     }
 
     @Singleton
     @Provides
-    fun provideTaskUiToTaskMapper(): TaskUiToTaskMapper{
-        return TaskUiToTaskMapperImpl()
+    fun provideTaskUiToTaskMapper(): TaskUiToTaskDomainMapper {
+        return TaskUiToTaskDomainMapperImpl()
     }
 
     @Singleton
     @Provides
-    fun provideTaskToTaskDbMapper(): TaskToTaskDbMapper{
+    fun provideTaskToTaskDbMapper(): TaskToTaskDbMapper {
         return TaskToTaskDbMapperImpl()
+    }
+
+    @Singleton
+    @Provides
+    fun provideProfileMapperUI() : ProfileMapperUI{
+        return ProfileMapperUIImpl()
+    }
+
+    @Singleton
+    @Provides
+    fun provideProfileMapperDB() : ProfileMapperDB {
+        return ProfileMapperDBImpl()
     }
 }
