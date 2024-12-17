d2df3dd8a9ebef8dda118f69644f5619dfe59473
Lapoushko <vadim.dovgiy@mail.ru>
2024-12-18 00:45:05 +0500

    update chip

--- app/src/main/java/com/example/android_app_sdvg/presentation/component/chip/CustomAssistChip.kt
+++ app/src/main/java/com/example/android_app_sdvg/presentation/component/chip/CustomAssistChip.kt
@@ -3,6 +3,7 @@ package com.example.android_app_sdvg.presentation.component.chip
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
@@ -25,12 +26,23 @@ fun CustomAssistChip(
     AssistChip(
         onClick = { chipActions.onClick() },
         label = { Text(chipActions.label) },
+        leadingIcon = {
+            if (chipActions.isAdded) {
+                Icon(
+                    modifier = Modifier.size(AssistChipDefaults.IconSize),
+                    imageVector = Icons.Filled.Close,
+                    contentDescription = "Localized description",
+                )
+            }
+        },
         trailingIcon = {
-            Icon(
-                modifier = Modifier.size(AssistChipDefaults.IconSize),
-                imageVector = Icons.Filled.KeyboardArrowDown,
-                contentDescription = "Localized description",
-            )
+            if (!chipActions.isAdded) {
+                Icon(
+                    modifier = Modifier.size(AssistChipDefaults.IconSize),
+                    imageVector = Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Localized description",
+                )
+            }
         },
         shape = RoundedCornerShape(40.dp)
     )
@@ -45,4 +57,5 @@ fun CustomAssistChipPreview(){
 class ChipActions(
     val label: String,
     val onClick: () -> Unit,
+    val isAdded: Boolean = false
 )
