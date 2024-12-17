11eba515746270a1616cd6da3cb4f4546add15a0
Lapoushko <vadim.dovgiy@mail.ru>
2024-12-18 00:45:20 +0500

    refactoring topbar

--- app/src/main/java/com/example/android_app_sdvg/presentation/component/CustomTopAppBar.kt
+++ app/src/main/java/com/example/android_app_sdvg/presentation/component/CustomTopAppBar.kt
@@ -9,6 +9,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 /**
@@ -20,7 +22,8 @@ fun CustomTopAppBar(
 ) {
     TopAppBar(modifier =
     Modifier
-        .padding(PaddingValues(top = 22.dp)),
+        .padding(PaddingValues(top = 0.dp))
+        .shadow(3.dp, spotColor = Color.DarkGray),
         title = {
             Text(
                 text = title
